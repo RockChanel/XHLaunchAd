1003f4bee617b065d4d36af6ae5e1d0a17c94fe3
selwyn <selwynbee@163.com>
2025-01-06 10:00:32 +0800

    feat：修改全屏判断

--- XHLaunchAd/XHLaunchAd/XHLaunchAdConst.h
+++ XHLaunchAd/XHLaunchAd/XHLaunchAdConst.h
@@ -15,10 +15,20 @@
 #define XH_ScreenW    [UIScreen mainScreen].bounds.size.width
 #define XH_ScreenH    [UIScreen mainScreen].bounds.size.height
 
-#define XH_IPHONEX  ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_IPHONEXR    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_IPHONEXSMAX    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_FULLSCREEN ((XH_IPHONEX || XH_IPHONEXR || XH_IPHONEXSMAX) ? YES : NO)
+//#define XH_IPHONEX  ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+//#define XH_IPHONEXR    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) : NO)
+//#define XH_IPHONEXSMAX    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) : NO)
+//#define XH_FULLSCREEN ((XH_IPHONEX || XH_IPHONEXR || XH_IPHONEXSMAX) ? YES : NO)
+
+#define XH_FULLSCREEN ({ \
+    BOOL isNotch = NO; \
+    if (@available(iOS 11.0, *)) { \
+        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow; \
+        CGFloat topInset = keyWindow.safeAreaInsets.top; \
+        isNotch = topInset > 20.0; \
+    } \
+    isNotch; \
+})
 
 
 #define XHISURLString(string)  ([string hasPrefix:@"https://"] || [string hasPrefix:@"http://"]) ? YES:NO
